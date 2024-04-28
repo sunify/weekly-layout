10510e3f7e123e7f00a2a6a8e96ebcea6a5c6c7d
Alex Lunyov <isuntc@gmail.com>
2024-04-28 20:15:37 +0200

    Index route, route for taking screenshot

--- index.ts
+++ index.ts
@@ -3,7 +3,6 @@ import { glob } from 'glob';
 import { searchSolution, type Node } from './layout';
 import { imageSize } from 'image-size';
 
-const PPI = 2;
 const OUTER_BORDER_W = 170;
 const OUTER_BORDER_H = 100;
 const WIDTH = 2970;
@@ -20,25 +19,25 @@ function serveImage(request: Request): Response {
 const p = await puppeteer.launch();
 
 function delay(ms: number) {
-  return new Promise(resolve => setTimeout(resolve, ms));
+  return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
-function renderImagesPage(images: Array<string>, nodes: Array<Node>) {
+function renderImagesPage(images: Array<string>, nodes: Array<Node>, { ppi = 1 }: { ppi: number }) {
   const nodesHtml = nodes
-  .map(
-    (node) => `
+    .map(
+      (node) => `
 <div
 class="node"
 style="
-  width: ${node.width * PPI}px;
-  height: ${node.height * PPI}px;
-  left: ${node.x * PPI}px;
-  top: ${node.y * PPI}px;
+  width: ${node.width * ppi}px;
+  height: ${node.height * ppi}px;
+  left: ${node.x * ppi}px;
+  top: ${node.y * ppi}px;
   "
 ><img src="http://localhost:${PORT}/${images[node.index as any]}" /></div>`
-  )
-  .join('');
-const html = `<!DOCTYPE html>
+    )
+    .join('');
+  const html = `<!DOCTYPE html>
 <html lang="en">
 <head>
 <meta charset="UTF-8">
@@ -47,23 +46,27 @@ const html = `<!DOCTYPE html>
 body {
 padding: 0;
 margin: 0;
+display: flex;
+align-items: center;
+justify-content: center;
+min-height: 100vh;
 }
 .border {
-width: ${WIDTH * PPI}px;
-height: ${HEIGHT * PPI}px;
-padding: ${OUTER_BORDER_H * PPI}px ${OUTER_BORDER_W * PPI}px;
+width: ${WIDTH * ppi}px;
+height: ${HEIGHT * ppi}px;
+padding: ${OUTER_BORDER_H * ppi}px ${OUTER_BORDER_W * ppi}px;
 background: #000;
 box-sizing: border-box;
 }
 .container {
-width: ${(WIDTH - OUTER_BORDER_W * 2) * PPI}px;
-height: ${(HEIGHT - OUTER_BORDER_H * 2) * PPI}px;
+width: ${(WIDTH - OUTER_BORDER_W * 2) * ppi}px;
+height: ${(HEIGHT - OUTER_BORDER_H * 2) * ppi}px;
 position: relative;
 }
 
 .node {
 position: absolute;
-padding: ${GAP * PPI}px;
+padding: ${GAP * ppi}px;
 box-sizing: border-box;
 }
 
@@ -71,22 +74,65 @@ box-sizing: border-box;
 width: 100%;
 height: 100%;
 }
+
+.submit {
+  position: fixed;
+  top: 0;
+  left: 0;
+  padding: 10px;
+  opacity: 0;
+}
+
+.submit:hover {
+  opacity: 1;
+}
 </style>
 </head>
 <body>
+<form class="submit js-submit">
+<button>Save screenshot</button>
+</form>
 <div class="border">
 <div class="container">${nodesHtml}</div>
 </div>
+<script>
+const form = document.querySelector('.js-submit');
+form.addEventListener('submit', async (e) => {
+  e.preventDefault();
+  const r = await fetch(window.location, {
+    method: 'post',
+    body: JSON.stringify({ nodes: ${JSON.stringify(nodes)} })
+  })
+  fetch('.')
+});
+</script>
 </body>
 </html>`;
 
   return html;
 }
 
-async function makeScreenshot(html: string, path: string) {
+function renderIndexPage(folders: Array<string>) {
+  const html = `<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+<ul>
+${folders.map((f) => `<li><a href="/${f}">${f}</a></li>`).join('')}
+</ul>
+</body>
+</html>`;
+
+  return html;
+}
+
+async function makeScreenshot(html: string, path: string, ppi: number) {
   const page = await p.newPage();
   await page.setContent(html);
-  await page.setViewport({ width: WIDTH * PPI, height: HEIGHT * PPI });
+  await page.setViewport({ width: WIDTH * ppi, height: HEIGHT * ppi });
   await page.screenshot({ path });
   await page.close();
 }
@@ -97,11 +143,30 @@ Bun.serve({
     if (url.pathname.endsWith('.jpg')) {
       return serveImage(request);
     }
-    const [date] = url.pathname.split('/').slice(1);
+    const [date] = url.pathname.split('/').slice(1).filter(Boolean);
+
+    if (date === undefined) {
+      const folders = (await glob('images/*')).map((f) =>
+        f.replace('images/', '')
+      );
+      const html = renderIndexPage(folders);
+      return new Response(html, { headers: { 'Content-Type': 'text/html' } });
+    }
 
     const images = await glob(`images/${date}/*.jpg`);
     if (images.length === 0) {
       return new Response('404', { status: 404 });
+    }
+
+    if (request.method.toLowerCase() === 'post') {
+      const body: { nodes: Array<Node> } = await request.json();
+      const html = renderImagesPage(images, body.nodes, { ppi: 2 });
+      await makeScreenshot(html, `pages/${date}.png`, 2);
+      return new Response(html, {
+        headers: {
+          'Content-Type': 'text/html',
+        },
+      });
     }
 
     const sizes = images.map((img) => imageSize(img));
@@ -111,9 +176,7 @@ Bun.serve({
       sizes.map(({ width, height }) => (width || 0) / (height || 0)),
       500
     );
-    const html = renderImagesPage(images, nodes);
-
-    await makeScreenshot(html, `pages/${date}.png`);
+    const html = renderImagesPage(images, nodes, { ppi: 0.3 });
 
     return new Response(html, {
       headers: {
@@ -123,5 +186,3 @@ Bun.serve({
   },
   port: PORT,
 });
-
-
